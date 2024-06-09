4e7faac670a020cf28057558232098afa528175c
etrl-z <emiliotorella@gmail.com>
2024-06-10 00:47:29 +0200

    initialize

---
+++ js/script.js
@@ -0,0 +1,35 @@
+debugger
+const startButton = document.getElementById('startButton');
+const textArea = document.getElementById('textInput');
+
+startButton.onclick = () => {
+    var speech = true;
+    window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+    if (!window.SpeechRecognition) {
+        alert('Your browser does not support Speech Recognition.');
+        return;
+    }
+
+    const recognition = new SpeechRecognition();
+    recognition.interimResults = true;
+
+    recognition.onresult = (e) => {
+        const transcript = e.results[0][0].transcript;
+        textArea.innerHTML = transcript;
+    }
+
+    recognition.onerror = (e) => {
+        console.error('Speech Recognition Error: ', e.error);
+    };
+
+    // recognition.onend = () => {
+    //     if (speech) {
+    //         recognition.start(); // Restart recognition if speech is still true
+    //     }
+    // };
+
+    if (speech) {
+        recognition.start();
+    }
+}
